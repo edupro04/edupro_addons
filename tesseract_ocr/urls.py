70e4f3b3242b08b1ba98ade88c55d73b6ec796be
edupro04 <162811888+edupro04@users.noreply.github.com>
2024-03-30 20:05:05 +0530

    Add files via upload

--- tesseract_ocr/urls.py
+++ tesseract_ocr/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('image_to_text/', views.image_to_text, name='image_to_text'),
     path('extract_text_from_image/', views.extract_text_from_image, name='extract_text_from_image'),
 ]
