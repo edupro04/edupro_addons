57eb5251a976dd4cdb2bae4e927c4110ef22c5d0
edupro04 <162811888+edupro04@users.noreply.github.com>
2024-03-30 19:37:37 +0530

    Add files via upload

--- edupro_addons/urls.py
+++ edupro_addons/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
+from tesseract_ocr import views as ans
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', ans.index, name='index'),
     re_path(r'^tesseract_ocr/', include('tesseract_ocr.urls'), name='tesseract_ocr'),
 ]
