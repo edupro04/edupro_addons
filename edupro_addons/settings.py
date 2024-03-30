5ace0e831c6ebc5a7089a82d78fc5f9efe940946
edupro04 <162811888+edupro04@users.noreply.github.com>
2024-03-30 14:08:55 +0530

    Add files via upload

--- edupro_addons/settings.py
+++ edupro_addons/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
