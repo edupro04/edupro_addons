09c407a9670e97869e476fb52fe3ba08a9981d16
edupro04 <162811888+edupro04@users.noreply.github.com>
2024-04-02 00:06:37 +0530

    Add files via upload

--- edupro_addons/settings.py
+++ edupro_addons/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+],
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://192.168.230.128:8000",
